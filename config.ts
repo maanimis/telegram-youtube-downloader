cdfa5fea91033379fa55be6d2945bca884ff9f67
maanimis <maani.meisam@gmail.com>
2024-02-09 01:03:36 +0330

    update

---
+++ config.ts
@@ -0,0 +1,14 @@
+const config = {
+  BOT_TOKEN: "6593257340:AAEUd71n-EOfjEqwiIlVrLIAHlqBrHa3qHo",
+  DATABASE_URL: "file:./dev.sqlite",
+  REDIS_HOST: "127.0.0.1",
+  REDIS_PORT: "6379",
+  DOMAIN_NAME: "dl.noobtrader.ir",
+  SERVER_PORT: "6780",
+  PROXY: "socks5://127.0.0.1:40000",
+  apiRoot: "http://127.0.0.1:8081",
+  // apiRoot: "https://apitel.nirvanagp.workers.dev",
+  // apiRoot:'https://api.telegram.org',
+};
+
+export default config;
